d13278b8fc5f7f38573f41f516e41eabe00e783e
ALEXANDRIS <92946182+GODMALEXANDRIS@users.noreply.github.com>
2026-03-13 17:34:22 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 def test_hamburguesa():
-ingrediente = "carne"
-assert ingrediente == "carne", "¡Falta la carne en la hamburguesa!"
-print("Test superado: La hamburguesa tiene carne.")
+    # Estas líneas ahora tienen 4 espacios de sangría
+    ingrediente = "carne"
+    assert ingrediente == "carne", "¡Falta la carne en la hamburguesa!"
+    print("Test superado: La hamburguesa tiene carne.")
+
 if __name__ == "__main__":
-test_hamburguesa()
+    test_hamburguesa()
